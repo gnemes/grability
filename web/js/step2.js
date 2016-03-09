12eb4c17126952564a8a90130f624a9af62016da
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-09 18:44:28 -0300

    Refactor step 2

--- web/js/step2.js
+++ web/js/step2.js
@@ -3,4 +3,7 @@ var pending = tc.quantity;
 $(function() {
     // Change label to button Add Matrix
     $("#matrix-add").html("Add Matrix ("+pending+" left)");
+    
+    // Hide next step button
+    $("#next-step").hide();
 });
