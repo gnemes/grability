f3d9481b41570f525c80823812a12ddbe46092d0
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-09 18:58:03 -0300

    Refactor step 2

--- web/js/step2.js
+++ web/js/step2.js
@@ -2,13 +2,54 @@ var pending = 0;
 var tc = null;
 
 function initStep2(tcInit) {
+    // Init test cases object
     tc = tcInit;
     
+    // Update test cases quantity pending
     pending = tc.quantity;
     
-    // Change label to button Add Matrix
-    $("#matrix-add").html("Add Matrix ("+pending+" left)");
+    // Update add matrix button
+    updateAddMatrixButton();
     
     // Hide next step button
     $("#next-step").hide();
 };
+
+$("#matrix-add").on("click", function( event ) {
+    // Get Matrix size value
+    var matrixSize = $("#matrix-size").val();
+
+    // Adds matrix to results panel and to test cases object
+    addMatrix(matrixSize);
+
+    // Get test cases left
+    pending = pending - 1;
+
+    // Change test cases remaining or hide add button
+    if (pending == 0) {
+        $('#matrix-add').hide();
+        $("#next-step").show();
+        $("#matrix-container").html("Done!");
+    } else {
+        updateAddMatrixButton();
+    }
+});
+
+function updateAddMatrixButton()
+{
+    // Change label to button Add Matrix
+    $("#matrix-add").html("Add Matrix ("+pending+" left)");
+}
+
+function addMatrix(size)
+{
+    // Create Matrix object
+    var mat = new Matrix(size, 0);
+    
+    // Add to test cases matrices collection
+    tc.addMatriz(mat);
+    
+    // Add matrix to results panel
+    var li = "<li class='list-group-item' size='"+size+"'>Matrix (1,1,1)...("+size+","+size+","+size+")</li>";
+    $("#matrix-list").append(li);
+}
