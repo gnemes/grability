11c1026d022bc6ea99f80d7c5b1d332d9d354241
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-09 18:36:16 -0300

    Refactor step 2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -59,7 +59,7 @@ class DefaultController extends Controller
             [
                 'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
                 'breadcum' => $breadcum,
-                'testCases' => $testCases,
+                'testCases' => addslashes($testCases),
             ]
         );
     }
