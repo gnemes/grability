dfcbf3112d41ad8151e4897da6324dc85c1b3367
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-09 16:43:46 -0300

    Refactor step 1

--- web/js/step1.js
+++ web/js/step1.js
@@ -1,9 +1,10 @@
-$("#next-step").on('click', function( event ) {
-    alert("Inicio");
-    var testCases = new TestCases();
-    testCases.quantity = $("#testsqty").val();
-    
-    var jsonTestCases = JSON.stringify(testCases);
-    
-    alert(jsonTestCases);
+$(function() {
+    $("#next-step").on('click', function( event ) {
+        var testCases = new TestCases();
+        testCases.quantity = $("#testsqty").val();
+
+        var jsonTestCases = JSON.stringify(testCases);
+
+        alert(jsonTestCases);
+    });
 });
