2c066603563bfb17ca5d5fa725e6cff93f9f3ced
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-09 19:24:41 -0300

    Refactor step 3

--- web/js/step3.js
+++ web/js/step3.js
@@ -1,15 +1,26 @@
 var pending = 0;
 var tc = null;
+var matrices = null;
+var currentMatrix = null;
 
 function initStep2(tcInit) {
     // Init test cases object
     tc = tcInit;
     
+    // Init matrices
+    matrices = tc.matrices;
+    
+    // Update current matrix
+    currentMatrix = matrices.shift();
+    
     // Update test cases quantity pending
     pending = tc.quantity;
     
     // Update add matrix operations button
-    updateAddMatrixOperationsButton(pending);
+    updateAddMatrixOperationsButton(currentMatrix.size);
+    
+    // Update add matrix button
+    updateAddMatrixButton();
     
     // Hide next step button
     $("#next-step").hide();
@@ -22,4 +33,14 @@ function updateAddMatrixOperationsButton(newSize)
     } else {
         $("#matrix-size-label").html("Operations for matrix ("+newSize+", "+newSize+", "+newSize+"): ");
     }
+}
+
+$("#next-step").on('click', function( event ) {
+    submitTestCase(tc);
+});
+
+function updateAddMatrixButton()
+{
+    // Change label to button Add Matrix
+    $("#matrix-add").html("Add Operations for Matrix ("+pending+" left)");
 }
