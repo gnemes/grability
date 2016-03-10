0b48416e7ba142e3b1035fb1df2ab6c1b100b13e
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-10 12:55:17 -0300

    Refactor step 3

--- web/js/main.js
+++ web/js/main.js
@@ -18,11 +18,11 @@ TestCases.prototype.init = function(jsonTestCases) {
     var tcTmp = jQuery.parseJSON(jsonTestCases);
     
     this.quantity = tcTmp.quantity;
-    
+alert(tcTmp.matrices);     
     var self = this;
     $.each(tcTmp.matrices, function( elem ) {
         var tmpMatrix = new Matrix(elem.size, elem.operations);
-alert(tmpMatrix.size);         
+        
         self.addMatrix(tmpMatrix);
     });
 }
