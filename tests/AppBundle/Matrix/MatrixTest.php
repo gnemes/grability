941a840fea1f2d4c8afe9c1c9f66b20f280a2c54
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-11 12:52:11 -0300

    Adding unit tests

--- tests/AppBundle/Matrix/MatrixTest.php
+++ tests/AppBundle/Matrix/MatrixTest.php
@@ -23,4 +23,22 @@ class MatrixTest extends \PHPUnit_Framework_TestCase
         
         $this->assertEquals($expected, $matrix->parseOperations($commands, 2, 2));
     }
+    
+    public function testInvalidLessOperationsQuantity()
+    {
+        // Matrix instance
+        $matrix = new Matrix();
+
+        $commands = "UPDATE 1 1 1 4\n"
+                . "UPDATE 2 1 2 5\n"
+                . "QUERY 1 1 1 2 2 2\n";
+        
+        $expected = array(
+            "errorCode" => 1, 
+            "errorString" => "Expected 4 commands, but 3 commands found.",
+            "data" => array()
+        );
+        
+        $this->assertEquals($expected, $matrix->parseOperations($commands, 2, 4));
+    }
 }
